6c89c5d4c618ee053603c1efb3ca0fa114289f55
Frederic <fredericlepichonpro@gmail.com>
2020-06-22 19:39:37 +0200

    wip affichage

--- app/routes/upload.js
+++ app/routes/upload.js
@@ -4,7 +4,7 @@ const base64Img = require('base64-img');
 app.post('/upload', (req, res) => {
   const {visuFinal} = req.body[0];
   const name = req.body[1].replace(/\..+$/, '');
-  base64Img.img(visuFinal, './public/', name ,function(err, filepath) {
+  base64Img.img(visuFinal, './public/image', name ,function(err, filepath) {
     const pathArr = filepath.split('/')
     const fileName = pathArr[pathArr.length - 1];
     console.log(fileName);
--- client/src/components/UniteSelected.vue
+++ client/src/components/UniteSelected.vue
@@ -4,16 +4,16 @@
       style="margin: auto; margin-top: 300px; background-color: white"
       :itemSize="100"
       :size="100"
-      :radius="230"
+      :radius="220"
       :angle-restriction="180"
-      :rotation="60"
+      :rotation="20"
       >
         <radial-menu-item 
-          v-for="(item, index) in Battletomes" 
+          v-for="(item, index) in Unites" 
           :key="index"
           @click="handleClick(item)"
         >
-          <v-avatar size="160">
+          <v-avatar size="200">
             <img
               :src="`${api}/image/${item.visuel}`"
               alt="battletomes"
@@ -21,37 +21,35 @@
           </v-avatar>
         </radial-menu-item>
       </radial-menu>
-      <UniteSelected ooo/>
   </v-container>
 </template>
 <script>
 import { RadialMenu,  RadialMenuItem } from 'vue-radial-menu'
 import axios from 'axios';
 import _ from 'lodash'; 
-import UniteSelected from '../components/UniteSelected'
 export default {
+  name: "UniteSelected",
   components: {
     RadialMenu,
     RadialMenuItem,
-    UniteSelected,
-    ooo: ''
   },
-  data:() => ({
-      ok: true,
+  data () {
+    return {
       lastClicked: '',
-      Battletomes:[''],
-      api: ''
-  }),
+      Unites:[],
+      ok: false
+    }
+  },
   methods: {
-  async getBattleTomes() {
+  async getUnites() {
     this.api = this.$api;
     axios({
-      url: `${this.api}/findBattletomes`,
+      url: `${this.api}/findUnites`,
       method: 'GET'
     })
-    .then(res => {
+    .then(async (res) => {
       res = res.data;
-      this.Battletomes = res;
+      this.Unites = res;
     })
     .catch(e => console.log(e));
   },
@@ -59,8 +57,8 @@ export default {
       this.lastClicked = item;
     },
   },
-  async mounted() {;
-    await this.getBattleTomes();
-  },  
+  async mounted() {
+      await this.getUnites();
+    },  
 }
 </script> 
