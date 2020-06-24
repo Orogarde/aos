e22bd73ec1bec037ea4ef0347010404efed3d184
Frederic <fredericlepichonpro@gmail.com>
2020-06-24 18:26:57 +0200

    wip affichage modele

--- client/src/components/UniteDetails.vue
+++ client/src/components/UniteDetails.vue
@@ -1,6 +1,164 @@
 <template>
   <v-container>
-    {{idUnite}}
+    <v-card>
+      <v-tabs
+        v-model="tab"
+        background-color="indigo"
+        dark
+      >
+        <v-tab
+          v-for="item in Unites"
+          :key="item.tab"
+        >
+          {{ item.nom }}
+        </v-tab>
+      </v-tabs>
+      <v-tabs-items v-model="tab">
+        <v-tab-item
+          v-for="item in Unites"
+          :key="item.tab"
+        >
+          <v-card flat>
+            <v-list>
+              <span class="title text-center">Profil :</span>
+              <v-list-item-group color="primary">
+                <v-list-item
+                  v-for="(model, i) in item.profils"
+                  :key="i"
+                >
+                  <v-list-item-content class="border-black subtitle-2">
+                    <v-list-item-title>Sauvegarde : {{model.sauvegarde}} +</v-list-item-title>
+                  </v-list-item-content>
+                   <v-list-item-content class="border-black subtitle-2">
+                    <v-list-item-title>Bravoure : {{model.bravoure}}</v-list-item-title>
+                  </v-list-item-content>
+                  <v-list-item-content class="border-black subtitle-2">
+                    <v-list-item-title>Mouvement : {{model.mouvement}} "</v-list-item-title>
+                  </v-list-item-content>
+                   <v-list-item-content class="border-black subtitle-2">
+                    <v-list-item-title>Blessure : {{model.blessure}}</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list-item-group>
+            </v-list>
+             <v-simple-table>
+              <template v-slot:default>
+                <thead>
+                  <tr>
+                    <th class="text-center">Armes</th>
+                    <th class="text-center">Portée</th>
+                    <th class="text-center">Attaques</th>
+                    <th class="text-center">Toucher</th>
+                    <th class="text-center">Blesser</th>
+                    <th class="text-center">Perf</th>
+                    <th class="text-center">Dégâts</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="model in item.armes" :key="model.nom">
+                    <td>{{ model.nom }}</td>
+                    <td>{{ model.portee }}</td>
+                    <td>{{ model.nb_attaque }}</td>
+                    <td>{{ model.touche }} +</td>
+                    <td>{{ model.blesse }} +</td>
+                    <td>{{ model.perforation }}</td>
+                    <td>{{ model.degat }}</td>
+                  </tr>
+                </tbody>
+              </template>
+            </v-simple-table>
+            <v-row justify="center">
+              <v-col  v-if="!empty(item.aptitude_commandements)"
+                cols="12"
+                sm="6"
+              >
+              <div class="title ">Aptitudes :</div>
+               <v-row justify="center">
+                  <v-expansion-panels popout>
+                    <v-expansion-panel
+                      v-for="(model,i) in item.aptitudes"
+                      :key="i"
+                    >
+                      <v-expansion-panel-header>{{model.nom}}</v-expansion-panel-header>
+                      <v-expansion-panel-content>
+                        {{model.effet}}
+                      </v-expansion-panel-content>
+                    </v-expansion-panel>
+                  </v-expansion-panels>
+                </v-row>
+              </v-col>
+              <v-col v-else
+                cols="12"
+                sm="12"
+              >
+             <div class="title">Aptitudes :</div>
+               <v-row justify="center">
+                  <v-expansion-panels popout>
+                    <v-expansion-panel
+                      v-for="(model,i) in item.aptitudes"
+                      :key="i"
+                    >
+                      <v-expansion-panel-header>{{model.nom}}</v-expansion-panel-header>
+                      <v-expansion-panel-content>
+                        {{model.effet}}
+                      </v-expansion-panel-content>
+                    </v-expansion-panel>
+                  </v-expansion-panels>
+                </v-row>
+              </v-col>
+              <v-col  v-if="!empty(item.aptitude_commandements)"
+                cols="12"
+                sm="6"
+              > 
+              <div class="title">Aptitudes de commandements :</div>
+                <v-row justify="center">
+                  <v-expansion-panels popout>
+                    <v-expansion-panel
+                      v-for="(model,i) in item.aptitude_commandements"
+                      :key="i"
+                    >
+                      <v-expansion-panel-header>{{model.nom}}</v-expansion-panel-header>
+                      <v-expansion-panel-content>
+                        {{model.effet}}
+                      </v-expansion-panel-content>
+                    </v-expansion-panel>
+                  </v-expansion-panels>
+                </v-row>
+              </v-col>
+            </v-row>
+             <v-row justify="center" v-if="!empty(item.effets)">
+                <div class="title">Tableau dégressif :</div>
+                  <v-expansion-panels popout>
+                    <v-expansion-panel>
+                      <!-- <v-expansion-panel-header @click="Tri(item.effets)">effets</v-expansion-panel-header> -->
+                      <v-expansion-panel-header>Tableau de dégâts</v-expansion-panel-header>
+                      <v-expansion-panel-content>
+                         <v-simple-table>
+                            <template v-slot:default>
+                              <thead>
+                                <tr>
+                                  <th class="text-center">Blessure</th>
+                                  <th class="text-center">Nom</th>
+                                  <th class="text-center">Valeur</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                <tr v-for="model in item.effets" :key="model.nom">
+                                  <td>{{ model.blessure }}</td>
+                                  <td>{{ model.nom }}</td>
+                                  <td>{{ model.valeur }}</td>
+                                </tr>
+                              </tbody>
+                            </template>
+                          </v-simple-table>
+                      </v-expansion-panel-content>
+                    </v-expansion-panel>
+                  </v-expansion-panels>
+                </v-row>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+  </v-card>
   </v-container>
 </template>
 <script>
@@ -19,6 +177,7 @@ export default {
   },
   data () {
     return {
+      tab: null,
       lastClicked: '',
       Unites:[''],
       api: '',
@@ -39,12 +198,32 @@ export default {
       console.log(res);
       res = res.data;
       this.Unites = res;
+      console.log(typeof this.Unites[0].effets);
     })
     .catch(e => console.log(e));
   },
   handleClick (item) {
       this.lastClicked = item;
     },
+  // Tri(item){
+  //   console.log(item);
+  //   var blessure = [];
+  //   const valeur = [];
+  //   item.forEach(function(element){
+  //    if(blessure.indexOf(element.blessure) === -1);
+  //    {
+  //      console.log(blessure);
+  //      console.log(element.blessure);
+  //     blessure.push(element.blessure);
+  //    }
+
+  //   });
+  //   console.log(blessure);
+  // },
+  empty(item)
+  {
+    return _.isEmpty(item);
+  }
   },
   async mounted() {
       await this.getUnites();
